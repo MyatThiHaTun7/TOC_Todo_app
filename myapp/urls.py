249da3e861217a66c1798d7471a799dc7e42a153
MyatTHiHaTun7 <myatthihatun78@gmail.com>
2025-08-12 20:29:56 +0630

    delete

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.TodoList, name="TodoList")
+    path('',views.TodoList, name="TodoList"),
+    path('create/',views.TodoCreate, name="TodoCreate"),
+    path('detail/<uuid:pk>/', views.TodoDetail, name="TodoDetail"),
+    path('update/<uuid:pk>/', views.TodoUpdate, name="TodoUpdate"),
+    path('delete/<uuid:pk>/', views.TodoDelete, name="TodoDelete")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import TodoModel
 # Create your views here.
 
+#List Function
 def TodoList(request):
     todo = TodoModel.objects.all()
     contex = {
         'todo' : todo
     }
     return render(request, 'index.html',contex)
+
+#Created Function
+def TodoCreate(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        completed = request.POST.get('completed') == 'on'
+
+        TodoModel.objects.create(
+            title=title,
+            description=description,
+            completed=completed,
+        )
+
+        return redirect('TodoList')
+    return render(request, 'todo_form.html')
+
+# Detail Function
+def TodoDetail(request,pk):
+    todo = get_object_or_404(TodoModel, pk=pk)
+    return render(request,'todo_detail.html', {'todo':todo})
+
+# Update Function
+def TodoUpdate(requset,pk):
+    todo = get_object_or_404(TodoModel, pk=pk)
+
+    if requset.method == 'POST':
+        todo.title = requset.POST.get('title')
+        todo.description = requset.POST.get('description')
+        todo.completed = requset.POST.get('completed') == 'on'
+        todo.save()
+        return redirect('TodoList')
+    return render(requset, 'todo_form.html', {'todo':todo})
+
+# Delete Function
+def TodoDelete(request, pk):
+    todo = get_object_or_404(TodoModel, pk=pk)
+
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('TodoList')
+    return render(request, 'index.html', {'todo':todo})
